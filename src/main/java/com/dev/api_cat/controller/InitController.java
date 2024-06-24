e3b7a1271563e8d124908edffd873a607bb12a8b
Drakoxw <dragon12xw@gmail.com>
2024-06-23 22:18:21 -0500

    feat: se cambia la base de datos a railway

--- src/main/java/com/dev/api_cat/controller/InitController.java
+++ src/main/java/com/dev/api_cat/controller/InitController.java
@@ -1,23 +1,16 @@
 package com.dev.api_cat.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.api_cat.persistence.models.CatBreedModel;
-import com.dev.api_cat.service.IApiCat;
 
 @RestController
 @RequestMapping("/test")
 public class InitController {
 
-    @Autowired
-    private IApiCat apiCatService;
 
     @GetMapping("/")
     public String home(){
