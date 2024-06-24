2c75c0b27485f7862f5b622ae8848dac8f51493a
Drakoxw <dragon12xw@gmail.com>
2024-06-23 20:06:01 -0500

    feat(CatBreedRepository): se mejora el query para cualquier coincidencia

--- src/main/java/com/dev/api_cat/persistence/repository/CatBreedRepository.java
+++ src/main/java/com/dev/api_cat/persistence/repository/CatBreedRepository.java
@@ -9,7 +9,7 @@ import com.dev.api_cat.persistence.models.CatBreedModel;
 
 public interface CatBreedRepository  extends MongoRepository<CatBreedModel, String> {
 
-    @Query("{ '$or': [ { 'uuid': ?0 }, { 'externalid': ?0 }, { 'name': ?0 }, { 'origin': ?0 }, { 'description': ?0 }, { 'altNames': ?0 }, { 'lifeSpan': ?0 }, { 'referenceImageId': ?0 } ] }")
+    @Query("{ '$or': [ { 'uuid': { $regex: ?0, $options: 'i' } }, { 'externalid': { $regex: ?0, $options: 'i' } }, { 'name': { $regex: ?0, $options: 'i' } }, { 'origin': { $regex: ?0, $options: 'i' } }, { 'description': { $regex: ?0, $options: 'i' } }, { 'altNames': { $regex: ?0, $options: 'i' } }, { 'lifeSpan': { $regex: ?0, $options: 'i' } }, { 'referenceImageId': { $regex: ?0, $options: 'i' } } ] }")
     Page<CatBreedModel> findByAnyField(String value, Pageable pageable);
     
 }
