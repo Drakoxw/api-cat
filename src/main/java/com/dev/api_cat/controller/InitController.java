bf945478d5212dca571a8bce101bd996162b4db3
Drakoxw <dragon12xw@gmail.com>
2024-06-23 21:47:53 -0500

    debug: /breeds

--- src/main/java/com/dev/api_cat/controller/InitController.java
+++ src/main/java/com/dev/api_cat/controller/InitController.java
@@ -1,14 +1,30 @@
 package com.dev.api_cat.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.api_cat.persistence.models.CatBreedModel;
+import com.dev.api_cat.service.IApiCat;
 
 @RestController
 public class InitController {
 
+    @Autowired
+    private IApiCat apiCatService;
+
     @RequestMapping("/")
     public String home(){
         return "Hello World!";
+    }
+
+    @RequestMapping("/breeds")
+    public ResponseEntity<List<CatBreedModel>> listBreeds() {
+        return new ResponseEntity<List<CatBreedModel>>(apiCatService.listAll(), HttpStatus.OK);
     }
 
 }
