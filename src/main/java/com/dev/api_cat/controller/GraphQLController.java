5864917984fd3a575cad6db916aea6595c3ffabe
Drakoxw <dragon12xw@gmail.com>
2024-06-24 12:27:28 -0500

    feat: se limpia el controllador

--- src/main/java/com/dev/api_cat/controller/GraphQLController.java
+++ src/main/java/com/dev/api_cat/controller/GraphQLController.java
@@ -1,6 +1,5 @@
 package com.dev.api_cat.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dev.api_cat.dto.CatBreedFullDataDto;
 import com.dev.api_cat.dto.ResponseDto;
